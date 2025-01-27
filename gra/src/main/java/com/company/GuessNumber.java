e010ea11e3d35e00566d037356dcb6fa7a4577e0
SaberLS <patryk.platek19@gmail.com>
2025-01-27 19:15:20 +0100

    GuessNum + Game/Guess Interfaces

--- gra/src/main/java/com/company/GuessNumber.java
+++ gra/src/main/java/com/company/GuessNumber.java
@@ -1,32 +1,53 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class GuessNumber extends GuessGame<Integer> {
-
+public class GuessNumber extends GuessGame<Integer, Integer> {
   public GuessNumber(int nb_chances, int answer, String name, String description) {
     super(nb_chances, answer, name, description);
   }
 
   public GuessNumber(int nb_chances, int answer) {
     super(nb_chances, answer, "Number Game", "Guess number");
+
+    this.wrongQuesses = new Integer[nb_chances];
+    this.correctQuesses = new Integer[1];
+
+    Arrays.fill(this.correctQuesses, null);
+    Arrays.fill(this.wrongQuesses, null);
   }
 
-  public void play() {
-    this.start();
+  public void guess(Integer guessed) {
+    this.tries++;
 
-    Scanner scr = new Scanner(System.in);
-    int input;
+    if (guessed == this.getAnswer()) {
+      this.state = State.WIN;
+      this.correctQuesses[0] = guessed;
+    } else {
+      int index = 0;
+      for (Integer element : this.wrongQuesses) {
+        if (element == null)
+          break; // Znalazłem element
+        index++;
+      }
 
-    while (this.state == State.PLAYING) {
-      System.out.println("Pozostałe próby :" + this.triesLeft());
-      System.out.print("Zgadnij liczbę");
-      input = scr.nextInt();
-      this.guess(input);
+      this.wrongQuesses[index] = guessed;
+
+      if (this.triesLeft() == 0) {
+        this.state = State.LOSE;
+      }
     }
+  }
 
-    scr.close();
-    String message = this.quit();
-    System.out.println(message);
+  public String attemptMessage() {
+    String str = "Zgadnij liczbę\nPozostałe próby:\t" + this.triesLeft() + "/" + this.getChances() + "\t\n"
+        + "Nie poprawne odpowiedzi " + Arrays.toString(this.wrongQuesses);
+    return str;
+  }
+
+  public Integer getUserInput(Scanner scr) {
+    Integer input = scr.nextInt();
+    return input;
   }
 }
